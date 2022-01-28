7fa14ce66ca7f7169b91dadb3862f5ebba506ec1
Winston Shields <winstonjshields@gmail.com>
2022-01-28 17:01:23 -0500

    update

---
+++ constants.php
@@ -0,0 +1,16 @@
+<?php
+
+define('SQL_SERVER', '');
+define('SQL_USERNAME', '');
+define('SQL_PASSWORD', '');
+define('DBNAME', 'etdsearch');
+define('EMAIL', 'etdfiguresearch@gmail.com');
+define('PASSWORD', 'dglnqmbqchdqzghb');
+define('SITE_KEY', '6LfDjzYaAAAAAOaIsUkEwOO3rK1GpMoJopgYJnQB');
+define('SECRET_KEY', '6LfDjzYaAAAAAE2Ehe8MZAsjMc9Upjw0EvSO9j5M');
+define('DISSERTATIONS_PATH', "/mnt/jwu/giacconi/etdrepo/");
+define('ETD_LOGS_PATH', "/var/www/html/secure_html/ETD_Logs/");
+define('ELASTICSEARCH_HOST', [""]);
+define('FIGURE_EXTRACTION_SERVICE_PATH', '/mnt/jwu/penzias/secure_html/sampanna/');
+define('ADMIN_PASSWORD', '313ctr1c1+y');
+define('ADMIN', 'admin');
